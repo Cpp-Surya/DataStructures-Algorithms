d7def9ca0ad54d80a7a46f2ce9fa005e6262b51d
Surya Saladi <surya.teja9988@gmail.com>
2025-10-29 21:24:38 +0530

    DSA: patterns practice complete

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -190,6 +190,303 @@ void pattern10(int n){
    }
 }
 
+void pattern11(int n){
+    /*
+        1
+        0 1
+        1 0 1
+        0 1 0 1
+        1 0 1 0 1
+    */
+    for(int i=0;i<n;i++){
+        int start;
+        if(i%2 == 0){
+            start = 1;
+        }else{
+            start = 0;
+        }
+        for(int j=0;j<=i;j++){
+            std::cout<<start<<" ";
+            start=1-start;
+        }
+        std::cout<<"\n";
+    }
+}
+
+void pattern12(int n){
+    /*
+        1      1
+        12    21
+        123  321
+        12344321
+        
+    */
+    for(int i=1;i<=n;i++){
+        //for number
+        for(int j=1;j<=i;j++){
+            std::cout<<j;
+        }
+        //for space
+        for(int j=1;j<=2*(n-i);j++){
+            std::cout<<" ";
+        }
+        //for number
+        for(int j=i;j>=1;j--){
+            std::cout<<j;
+        }
+        std::cout<<"\n";
+    }
+}
+
+void pattern13(int n){
+    /*
+        1 
+        2 3 
+        4 5 6 
+        7 8 9 10 
+        11 12 13 14 15
+    */
+    int start=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            std::cout<<start++<<" ";
+        }
+        std::cout<<"\n";
+    }
+}
+
+void pattern14(int n){
+    /*
+        A
+        AB
+        ABC
+        ABCD
+        ABCDE
+    */
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            std::cout<<(char)(65+j);
+        }
+        std::cout<<"\n";
+    }
+}
+
+void pattern15(int n){
+    /*
+        ABCDE
+        ABCD
+        ABC
+        AB
+        A
+    */
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            std::cout<<(char)(65+j);
+        }
+        std::cout<<"\n";
+    }
+}
+
+void pattern16(int n){
+    /*
+        A
+        BB
+        CCC
+        DDDD
+        EEEEE
+    */
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            std::cout<<(char)(65+i);
+        }
+        std::cout<<"\n";
+    }
+}
+
+void pattern17(int n){
+    /*
+         A   
+        ABA  
+       ABCBA 
+      ABCDCBA
+    */
+    for(int i=0;i<n;i++){
+        //for space
+        for(int j=0;j<n-i-1;j++){
+            std::cout<<" ";
+        }
+        
+        //for characters
+        for(int j=0;j<=i;j++){
+            std::cout<<(char)(65+j);
+        }
+        
+        for(int j=i;j>0;j--){
+            std::cout<<(char)(65+j-1);
+        }
+        
+        //for space
+        for(int j=0;j<n-i-1;j++){
+            std::cout<<" ";
+        }
+        std::cout<<"\n";
+    }
+}
+
+void pattern18(int n){
+    /*
+        E 
+        D E 
+        C D E 
+        B C D E 
+        A B C D E
+    */
+    for(int i=0;i<n;i++){
+        char ch = 'A'+n-i-1;
+        for(int j=0;j<=i;j++){
+            std::cout<<ch++<<" ";
+        }
+        std::cout<<"\n";
+    }
+}
+
+void pattern19(int n){
+    /*
+        **********
+        ****  ****
+        ***    ***
+        **      **
+        *        *
+        *        *
+        **      **
+        ***    ***
+        ****  ****
+        **********
+    */
+    int total_row_count = 2*n;
+    int space=0;
+    for(int i=0;i<total_row_count;i++){
+        if(i<(total_row_count/2)){
+            //for stars
+            for(int j=0;j<n-i;j++){
+                std::cout<<"*";
+            }
+            //for space
+            for(int j=0;j<space;j++){
+                std::cout<<" ";
+            }
+            //for stars
+            for(int j=0;j<n-i;j++){
+                std::cout<<"*";
+            }
+            space=space+2;
+        }else{
+            space=space-2;
+            //for stars
+            for(int j=0;j<i-n+1;j++){
+                std::cout<<"*";
+            }
+            //for space
+            for(int j=0;j<space;j++){
+                std::cout<<" ";
+            }
+            //for stars
+            for(int j=0;j<i-n+1;j++){
+                std::cout<<"*";
+            }
+        }
+        std::cout<<"\n";
+    }
+}
+
+void pattern20(int n){
+    /*
+        *        *
+        **      **
+        ***    ***
+        ****  ****
+        **********
+        ****  ****
+        ***    ***
+        **      **
+        *        *
+    */
+    int total_row_count = 2*n-1;
+    for(int i=0;i<total_row_count;i++){
+        if(i<n){
+            //for stars
+            for(int j=0;j<=i;j++){
+                std::cout<<"*";
+            }
+            //for space
+            for(int j=0;j<2*(n-i)-2;j++){
+                std::cout<<" ";
+            }
+            //for stars
+            for(int j=0;j<=i;j++){
+                std::cout<<"*";
+            }
+        }else{
+            //for stars
+            for(int j=0;j<2*n-i-1;j++){
+                std::cout<<"*";
+            }
+            //for space
+            for(int j=0;j<2*(i-n)+2;j++){
+                std::cout<<" ";
+            }
+            //for stars
+            for(int j=0;j<2*n-i-1;j++){
+                std::cout<<"*";
+            }
+        }
+        std::cout<<"\n";
+        
+    }
+}
+
+void pattern21(int n){
+    /*
+        *****
+        *   *
+        *   *
+        *   *
+        *****
+    */
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 || j==0 || i==n-1 || j==n-1){
+                std::cout<<"*";
+            }else{
+                std::cout<<" ";
+            }
+        }
+        std::cout<<"\n";
+    }
+}
+
+void pattern22(int n){
+    /*
+        4444444
+        4333334
+        4322234
+        4321234
+        4322234
+        4333334
+        4444444
+    */
+    for(int i=0;i<2*n-1;i++){
+        for(int j=0;j<2*n-1;j++){
+            int top = i;
+            int left = j;
+            int right = (2*n-2)-j;
+            int down = (2*n-2)-i;
+            std::cout<<(n-std::min(std::min(top,down),std::min(left,right)));
+        }
+        std::cout<<"\n";
+    }
+}
+
 int main(){
     std::cout<<"-----------------\n";
     int n;
@@ -205,7 +502,20 @@ int main(){
     // pattern7(n);
     // pattern8(n);
     // pattern9(n);
-    pattern10(n);
+    // pattern10(n);
+    // pattern11(n);
+    // pattern12(n-1);
+    // pattern13(n);
+    // pattern14(n);
+    // pattern15(n);
+    // pattern16(n);
+    // pattern17(n-1);
+    // pattern18(n);
+    // pattern19(n);
+    // pattern20(n);
+    // pattern21(n);
+    pattern22(n-1);
+
     std::cout<<"-----------------\n";
     return 0;
 }
